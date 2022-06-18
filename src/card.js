eef0c7d221a41b1ace090f53464cce6ba2f1296c
chandrakumargb <chandrakumargb7@gmail.com>
2022-06-18 23:56:16 +0530

    BREAKING BAD

---
+++ src/card.js
@@ -0,0 +1,19 @@
+import styles from "./card.module.css";
+const Card = (props) => {
+  console.log("props", props);
+  return (
+    <div className={styles.center}>
+      <div className={styles.container}>
+        <div className={styles.card}>  
+          <img src={props.img} alt="" width={250} height={250}></img>
+          <h3> Name:{props.name}</h3>
+          <p key={props.char_id}></p>
+          <p>Date Of Birth: {props.birthday}</p>
+          <p> Nickname :{props.nickname}</p>
+          <p>Occupation:{props.occupation}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Card;
